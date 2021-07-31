93ee31aaa7da19b6ba4b06e1ea483aee9930b3de
ErfanEbrahimiBazaz <erfan.ebrahimibazaz@gmail.com>
2021-07-31 03:48:19 +0200

    Product List index and edit page

---
+++ WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DB;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        //public ProductController()
+        //{
+        //    var products = InMemoryDB.Products;
+        //}
+        List<Product> products = InMemoryDB.Products;
+        public IActionResult Index()
+        {
+
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            // Parameter name in view and action must be the same. It is safer to use <a> tag like  <a href="/Product/Edit/@item.ProductId">Edit</a> as it doesn't use a parameter name.
+            //index = InMemoryDB.Products.FindIndex(p => p.Equals(model));
+            var model = InMemoryDB.Products.FirstOrDefault(p => p.ProductId.Equals(id));
+            //if (model == null)
+            //{
+            //    return RedirectToAction("Index");
+            //}
+            return View(model);
+        }
+
+        #region create
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            InMemoryDB.Products.Add(model);
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+    }
+}
---
+++ WebApplication1/WebApplication1/DB/InMemoryDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.DB
+{
+    public static class InMemoryDB
+    {
+        public static List<Product> Products = new List<Product>
+        {
+            new Product{ ProductId = 1, ProductName = "Product 1", CategoryId = 1, UnitPrice = 100},
+            new Product{ ProductId = 2, ProductName = "Product 2", CategoryId = 1, UnitPrice = 100},
+            new Product{ ProductId = 3, ProductName = "Product 3", CategoryId = 1, UnitPrice = 100},
+            new Product{ ProductId = 4, ProductName = "Product 4", CategoryId = 1, UnitPrice = 100},
+            new Product{ ProductId = 5, ProductName = "Product 5", CategoryId = 1, UnitPrice = 100},
+            new Product{ ProductId = 6, ProductName = "Product 6", CategoryId = 1, UnitPrice = 100},
+            new Product{ ProductId = 7, ProductName = "Product 7", CategoryId = 1, UnitPrice = 100},
+            new Product{ ProductId = 8, ProductName = "Product 8", CategoryId = 1, UnitPrice = 100},
+            new Product{ ProductId = 9, ProductName = "Product 9", CategoryId = 1, UnitPrice = 100},
+            new Product{ ProductId = 10, ProductName = "Product 10", CategoryId = 1, UnitPrice = 100},
+        };
+
+        public static List<Category> Categories = new List<Category>
+        {
+            new Category{ CategoryId = 1, CategoryName = "Shrimp", CategoryDescription = "Seafood"},
+            new Category{ CategoryId = 2, CategoryName = "Squeed", CategoryDescription = "Seafood"},
+            new Category{ CategoryId = 3, CategoryName = "Date", CategoryDescription = "Fruite"},
+            new Category{ CategoryId = 4, CategoryName = "Dried Date", CategoryDescription = "Dried Fruit"}
+        };
+    }
+}
