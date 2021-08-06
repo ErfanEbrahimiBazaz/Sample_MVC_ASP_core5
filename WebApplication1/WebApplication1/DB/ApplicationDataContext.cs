7d1b1f61294d15c2356fe8d48d98f1e715c7c0f7
ErfanEbrahimiBazaz <erfan.ebrahimibazaz@gmail.com>
2021-08-06 22:24:25 +0200

    Generate DB by code first

---
+++ WebApplication1/WebApplication1/DB/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.DB
+{
+    public class ApplicationDataContext: DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options): base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Migrations/20210806202104_initialization.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class initialization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Organization = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactPerson = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nationality = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
