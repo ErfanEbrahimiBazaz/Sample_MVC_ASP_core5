3149711a456f379da50c1a94c39e343c1f008f44
ErfanEbrahimiBazaz <erfan.ebrahimibazaz@gmail.com>
2021-08-08 02:12:26 +0200

    Adding resources file to make web site bilingual

--- WebApplication1/WebApplication1/Models/Customer.cs
+++ WebApplication1/WebApplication1/Models/Customer.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using RefrenceLib;
+
 
 namespace WebApplication1.Models
 {
     public class Customer
     {
+        [Key]
+        [Display(Name = nameof(Titles.CustomerId), ResourceType = typeof(Titles))]
         public int CustomerId { get; set; }
+        [Required(ErrorMessageResourceName = nameof(ErrorMessages.Required), ErrorMessageResourceType = typeof(ErrorMessages))]
+        [Display(Name = nameof(Titles.Organization), ResourceType = typeof(Titles))]
         public string Organization { get; set; }
-        public string ContactPerson{ get; set; }
+        [Display(Name = nameof(Titles.ContactPerson), ResourceType = typeof(Titles))]
+        public string ContactPerson { get; set; }
+        [Display(Name = nameof(Titles.Address), ResourceType = typeof(Titles))]
         public string Address { get; set; }
+        [Display(Name = nameof(Titles.Nationality), ResourceType = typeof(Titles))]
         public string Nationality { get; set; }
     }
 }
